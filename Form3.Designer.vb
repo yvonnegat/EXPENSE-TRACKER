d9017d0649bf91542271f7802da56109f6929ecb
yvonnegat <114511715+yvonnegat@users.noreply.github.com>
2024-04-02 16:47:07 +0300

    Add files via upload

---
+++ Form3.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        SIGNUPbtn = New Button()
+        usernametextbox = New TextBox()
+        passwordtextbox = New TextBox()
+        signuplabel = New Label()
+        Usernamelabel = New Label()
+        Passwordlabel = New Label()
+        SuspendLayout()
+        ' 
+        ' SIGNUPbtn
+        ' 
+        SIGNUPbtn.Location = New Point(250, 210)
+        SIGNUPbtn.Name = "SIGNUPbtn"
+        SIGNUPbtn.Size = New Size(148, 52)
+        SIGNUPbtn.TabIndex = 0
+        SIGNUPbtn.Text = "Sign Up"
+        SIGNUPbtn.UseVisualStyleBackColor = True
+        ' 
+        ' usernametextbox
+        ' 
+        usernametextbox.ForeColor = SystemColors.InactiveCaption
+        usernametextbox.Location = New Point(271, 52)
+        usernametextbox.Name = "usernametextbox"
+        usernametextbox.Size = New Size(99, 23)
+        usernametextbox.TabIndex = 1
+        usernametextbox.Text = "username"
+        ' 
+        ' passwordtextbox
+        ' 
+        passwordtextbox.ForeColor = SystemColors.InactiveCaption
+        passwordtextbox.Location = New Point(270, 118)
+        passwordtextbox.Name = "passwordtextbox"
+        passwordtextbox.Size = New Size(100, 23)
+        passwordtextbox.TabIndex = 3
+        passwordtextbox.Text = "password"
+        ' 
+        ' signuplabel
+        ' 
+        signuplabel.AutoSize = True
+        signuplabel.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        signuplabel.Location = New Point(305, 18)
+        signuplabel.Name = "signuplabel"
+        signuplabel.Size = New Size(46, 15)
+        signuplabel.TabIndex = 4
+        signuplabel.Text = "sign up"
+        ' 
+        ' Usernamelabel
+        ' 
+        Usernamelabel.AutoSize = True
+        Usernamelabel.Location = New Point(185, 60)
+        Usernamelabel.Name = "Usernamelabel"
+        Usernamelabel.Size = New Size(68, 15)
+        Usernamelabel.TabIndex = 5
+        Usernamelabel.Text = "User Name:"
+        ' 
+        ' Passwordlabel
+        ' 
+        Passwordlabel.AutoSize = True
+        Passwordlabel.Location = New Point(185, 126)
+        Passwordlabel.Name = "Passwordlabel"
+        Passwordlabel.Size = New Size(60, 15)
+        Passwordlabel.TabIndex = 7
+        Passwordlabel.Text = "Password:"
+        ' 
+        ' Form3
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Passwordlabel)
+        Controls.Add(Usernamelabel)
+        Controls.Add(signuplabel)
+        Controls.Add(passwordtextbox)
+        Controls.Add(usernametextbox)
+        Controls.Add(SIGNUPbtn)
+        Name = "Form3"
+        Text = "Sign Up Page"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents SIGNUPbtn As Button
+    Friend WithEvents usernametextbox As TextBox
+    Friend WithEvents passwordtextbox As TextBox
+    Friend WithEvents signuplabel As Label
+    Friend WithEvents Usernamelabel As Label
+    Friend WithEvents Passwordlabel As Label
+End Class
---
+++ Form3.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Data.OleDb
+
+Public Class form3
+    Dim connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Administrator\Downloads\expenseTracker.accdb;"
+
+    Private Sub SignUpForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Black
+        Usernamelabel.ForeColor = Color.White
+        Passwordlabel.ForeColor = Color.White
+        signuplabel.ForeColor = Color.Blue
+        SIGNUPbtn.BackColor = Color.Blue
+    End Sub
+
+    Private Sub SIGNUPbtn_Click(sender As Object, e As EventArgs) Handles SIGNUPbtn.Click
+        Dim username As String = usernametextbox.Text
+        Dim password As String = passwordtextbox.Text
+
+        ' Check if username and password are not empty
+        If String.IsNullOrWhiteSpace(username) Or String.IsNullOrWhiteSpace(password) Then
+            MessageBox.Show("Please enter both username and password.")
+            Return
+        End If
+
+        ' Check if username already exists
+        If UsernameExists(username) Then
+            MessageBox.Show("Username already exists. Please choose a different one.")
+            Return
+        End If
+
+        ' Create new user
+        If CreateUser(username, password) Then
+            MessageBox.Show("User created successfully!")
+            ' Optionally clear the textboxes after successful registration
+            usernametextbox.Clear()
+            passwordtextbox.Clear()
+
+            Dim form4 As New Form4()
+            form4.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Failed to create user. Please try again.")
+        End If
+    End Sub
+
+    Private Function UsernameExists(username As String) As Boolean
+        Using connection As New OleDbConnection(connectionString)
+            connection.Open()
+
+            Dim query As String = "SELECT COUNT(*) FROM userDetails WHERE Username = @Username"
+            Using command As New OleDbCommand(query, connection)
+                command.Parameters.AddWithValue("@Username", username)
+
+                Dim count As Integer = Convert.ToInt32(command.ExecuteScalar())
+                Return count > 0
+            End Using
+        End Using
+    End Function
+
+    Private Function CreateUser(username As String, password As String) As Boolean
+        Using connection As New OleDbConnection(connectionString)
+            connection.Open()
+
+            Dim query As String = "INSERT INTO userDetails (Username, [Password]) VALUES (@Username, @Password)"
+            Using command As New OleDbCommand(query, connection)
+                command.Parameters.AddWithValue("@Username", username)
+                command.Parameters.AddWithValue("@Password", password)
+
+                Dim rowsAffected As Integer = command.ExecuteNonQuery()
+                Return rowsAffected > 0
+            End Using
+        End Using
+    End Function
+End Class
